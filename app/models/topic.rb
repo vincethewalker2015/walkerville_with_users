f102561493fb2112f948f526179fd25b296c7874
vincentbwalker <vincethewalker@gmail.com>
2018-10-25 14:10:55 +0000

    Added extra links from users to blogs

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   belongs_to :user
-  validates :user_id, presence: true
+  #validates :user_id, presence: true
   
   mount_uploader :picture, PictureUploader
   
